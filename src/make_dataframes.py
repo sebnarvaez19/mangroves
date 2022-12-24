28960f0bc1391f40476686a9d0f29ae3a3a5b42f
srnarvaez <sebnarvaez19@gmail.com>
2022-12-24 01:01:09 -0500

    Make one dataframe instead of one by variable

--- src/make_dataframes.py
+++ src/make_dataframes.py
@@ -12,32 +12,40 @@ plt.style.use("src/style.mplstyle")
 lagoons = ["mallorquin", "totumo", "virgen"]
 path_nc = "data/processed/{}_ndvi_temperature.nc"
 path_shp = "data/shapefile/mangrove_forests.shp"
-path_csv = "data/processed/{}_mean_ndvi_temperature.csv"
+path_csv = "data/processed/mean_ndvi_temperature.csv"
 
 forests = gpd.read_file(path_shp)
+data = []
 
 for lagoon in lagoons:
-    data = xarray.open_dataset(path_nc.format(lagoon), decode_coords="all")
+    ic = xarray.open_dataset(path_nc.format(lagoon), decode_coords="all")
     roi = forests[forests.key == lagoon].geometry
 
-    data = data.rio.clip(roi, all_touched=False)
+    ic = ic.rio.clip(roi, all_touched=False)
 
     df = pd.DataFrame({
-        "NDVI": data["NDVI"].mean(dim=["latitude", "longitude"]).to_series(),
-        "Temperature": data["Surface Temperature"].mean(dim=["latitude", "longitude"]).to_series(),
-        "Count": data["NDVI"].count(dim=["latitude", "longitude"]).to_series()
+        "NDVI": ic["NDVI"].mean(dim=["latitude", "longitude"]).to_series(),
+        "Temperature": ic["Surface Temperature"].mean(dim=["latitude", "longitude"]).to_series(),
+        "Count": ic["NDVI"].count(dim=["latitude", "longitude"]).to_series()
     }).resample("m").mean()
 
-    mask = df.Count < df.Count.max()*0.1
+    df.Count[df.Count.isna()] = 0
+    df["PixelPercentage"] = df.Count/df.Count.max()*100
+    df["Lagoon"] = lagoon
+
+    mask = df.PixelPercentage < 0.10
 
     df.NDVI[mask] = np.nan
     df.Temperature[mask] = np.nan
 
+    df.NDVI = na_seadec(df.NDVI)
+    df.Temperature = na_seadec(df.Temperature)
+
     df = df.drop("Count", axis=1)
-    
-    df["NDVI_interpolated"] = na_seadec(df.NDVI)
-    df["Temperature_interpolated"] = na_seadec(df.Temperature)
-
     df = df.dropna(axis=0, how="all")
+    df = df[df.index >= np.datetime64("2001-01-01")]
 
-    df.to_csv(path_csv.format(lagoon))
+    data.append(df)
+
+data = pd.concat(data)
+data.to_csv(path_csv)
