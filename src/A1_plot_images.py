721da775b5ea7060c9a7719d1028e27fb856bad8
srnarvaez <sebnarvaez19@gmail.com>
2022-12-24 23:29:52 -0500

    Appendix 1: Plot all images

--- src/A1_plot_images.py
+++ src/A1_plot_images.py
@@ -4,11 +4,12 @@ import geemap
 import matplotlib.pyplot as plt
 
 from geemap import cartoee
+from statgis.landsat_functions import landsat_scaler
 from functions.gee_processing import *
 
 ee.Initialize()
 
-# %% Define constants
+# %% Define constants and plot options
 path = "./appendix/all_images/{}.png"
 
 lagoons = ["mallorquin", "totumo", "virgen"]
@@ -26,19 +27,19 @@ forests = ee.FeatureCollection("projects/ee-sebnarvaez-mangroves/assets/forests"
 
 # %% Load, scale and rename Landsat 5, 7 and 8 image collections
 L5 = (
-    ee.ImageCollection("LANDSAT/LT05/C02/T1_L2").map(image_scaler)
+    ee.ImageCollection("LANDSAT/LT05/C02/T1_L2").map(landsat_scaler)
                                                 .map(renamer7)
                                                 .filter(ee.Filter.calendarRange(1996, 1998, "year"))
 )
 
 L7 = (
-    ee.ImageCollection("LANDSAT/LE07/C02/T1_L2").map(image_scaler)
+    ee.ImageCollection("LANDSAT/LE07/C02/T1_L2").map(landsat_scaler)
                                                 .map(renamer7)
                                                 .filter(ee.Filter.calendarRange(1999, 2013, "year"))
 )
 
 L8 = (
-    ee.ImageCollection("LANDSAT/LC08/C02/T1_L2").map(image_scaler)
+    ee.ImageCollection("LANDSAT/LC08/C02/T1_L2").map(landsat_scaler)
                                                 .map(renamer8)
                                                 .filter(ee.Filter.calendarRange(2014, 2021, "year"))
 )
@@ -48,14 +49,20 @@ IC = L5.merge(L7.merge(L8))
 
 # %% Loop over the image collection to plot all images
 for lagoon in lagoons:
+    # Get the forest of interest
     roi = forests.filter(ee.Filter.eq("key", lagoon)).first().geometry()
+    
+    # Filter images that intersect with the forest of interest
     ICF = IC.filterBounds(roi)
     
+    # Extract images dates to loop on they
     dates = ICF.reduceColumns(ee.Reducer.toList(), ["DATE_ACQUIRED"]).get("list").getInfo()
 
     for date in dates:
+        # Filter images by date and make their mean
         img = ICF.filter(ee.Filter.eq("DATE_ACQUIRED", date)).mean()
 
+        # Plot with cartoee
         fig = plt.figure()
 
         ax = cartoee.get_map(img, vis_params=vis, region=zooms[lagoon])
