216175ca5134290022aea3a7b0e7259f9fa39d5d
srnarvaez <sebnarvaez19@gmail.com>
2022-12-25 00:53:43 -0500

    Comment functions

--- src/functions/gee_processing.py
+++ src/functions/gee_processing.py
@@ -15,9 +15,11 @@ def renamer7(img: ee.Image) -> ee.Image:
     img : ee.Image
         Image with bands filtereds and renamed.
     """
+    # Define the original and the new names
     ori_names = ["SR_B1", "SR_B2", "SR_B3", "SR_B4", "ST_B6"]
     new_names = ["BLUE", "GREEN", "RED", "NIR", "TEMPERATURE"]
     
+    # Select the bands with the original names an rename them
     img = img.select(ori_names).rename(new_names)
 
     return img
@@ -37,9 +39,11 @@ def renamer8(img: ee.Image) -> ee.Image:
     img : ee.Image
         Image with bands filtereds and renamed.
     """
+    # Define the original and the new names
     ori_names = ["SR_B2", "SR_B3", "SR_B4", "SR_B5", "ST_B10"]
     new_names = ["BLUE", "GREEN", "RED", "NIR", "TEMPERATURE"]
     
+    # Select the bands with the original names an rename them
     img = img.select(ori_names).rename(new_names)
 
     return img
@@ -58,9 +62,13 @@ def calc_ndvi(img: ee.Image) -> ee.Image:
     img : ee.Image
         Image with NDVI calculated.
     """
+    # Define the NDVI formula
     f = "(b('NIR')-b('RED'))/(b('NIR')+b('RED'))"
+    
+    # Calculate the NDVI
     ndvi = img.expression(f).rename("NDVI")
 
+    # Add NDVI band to image
     img = img.addBands(ndvi)
 
     return img
