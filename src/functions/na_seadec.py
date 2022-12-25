278e81b19613dcfdd46352c07371bfd26bb356e2
srnarvaez <sebnarvaez19@gmail.com>
2022-12-25 14:16:35 -0500

    function to interpolate seasonal component

--- src/functions/na_seadec.py
+++ src/functions/na_seadec.py
@@ -1,6 +1,9 @@
+import numpy as np
 import pandas as pd
 
-def na_seadec(x: pd.Series, period: int=13, method: str="linear") -> pd.Series:
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+def na_seadec(x: pd.Series, method: str="linear", model: str="additive") -> pd.Series:
     """
     Function to interpolate the NaN values in a series but without affect
     the long term trend and the seasonal component.
@@ -16,13 +19,18 @@ def na_seadec(x: pd.Series, period: int=13, method: str="linear") -> pd.Series:
     x : pd.Series
         Series of interest with the NaN values.
 
-    period : int
-        Period of the time series to calculate the long term trend.
-
     method : str
         Method of interpolation to fill the NaN values in the anomalies.
         the full list of option coul be found in pandas.Series.interpolate()
-        method: https://pandas.pydata.org/docs/reference/api/pandas.Series.interpolate.html
+        method: 
+        https://pandas.pydata.org/docs/reference/api/pandas.Series.interpolate.html
+
+    model : str
+        Additive or Multiplicative. The kind of model to decompose the 
+        time series. To read more about the difference between the two models
+        could be found in statsmodels.tsa.seasonal.seasonal_decompose()
+        function documentation:
+        https://www.statsmodels.org/dev/generated/statsmodels.tsa.seasonal.seasonal_decompose.html
 
     Returns
     -------
@@ -30,34 +38,35 @@ def na_seadec(x: pd.Series, period: int=13, method: str="linear") -> pd.Series:
         Series interpolated.
     """
 
-    # Get the long term trend with a rolling mean
-    trend = x.rolling(window=period, min_periods=1, center=True).mean()
-    
-    # Remove the trend
-    detrend = x - trend
-    
-    # Calculate the seasonal component with a mean by month
-    monthly_mean = detrend.groupby(detrend.index.month).mean()
-    
-    # Create the anomalies series
-    anomalies = detrend.copy()
+    # Get the time to reconstruct the series
+    time = x.index
 
-    # Extract the months from the series
-    months = anomalies.index.month
+    # Find the NaN values
+    mask = x.isna()
 
-    # Iterate over the months and motlhy mean data and remove it
-    # from detrended data
-    for i, v in zip(monthly_mean.index, monthly_mean):
-        anomalies[months == i] = anomalies[months == i] - v
+    # Interpolate the original series
+    interpolated = x.interpolate(method=method)
 
-    # Interpolate anomalies
-    anomalies = anomalies.interpolate(method=method)
+    # Decompose the series with seasonal_decompose from statsmodels
+    components = seasonal_decompose(interpolated, model=model, extrapolate_trend="freq")
 
-    # Restore de seasonal component
-    for i, v in zip(monthly_mean.index, monthly_mean):
-        anomalies[months == i] = anomalies[months == i] + v
-    
-    # Restore the long term trend
-    anomalies = anomalies + trend
+    # Sum the trend with the residuals to get the series without 
+    # seasonality
+    ts_no_seasonal = components.trend + components.resid
 
-    return anomalies
+    # Restor the NaN values
+    ts_no_seasonal[mask] = np.nan
+
+    # Create the second series to interpolate data without seasonality
+    x2 = pd.Series(data=ts_no_seasonal, index=time)
+
+    # Interpolate TS without seasonality
+    x2 = x2.interpolate(method=method)
+
+    # Add the seasonality to the interpolated data
+    x2 = x2 + components.seasonal
+
+    # Fill the NaN with the interpolated data
+    x[mask] = x2[mask]
+
+    return x
