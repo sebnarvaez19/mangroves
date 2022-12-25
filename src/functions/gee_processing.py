d3062cacb902f6d1e3a51f6cbb4f7c5aca610069
srnarvaez <sebnarvaez19@gmail.com>
2022-12-25 00:38:31 -0500

    Add types to the functions parameters and docstrings to the functions

--- src/functions/gee_processing.py
+++ src/functions/gee_processing.py
@@ -1,6 +1,20 @@
 import ee
 
-def renamer7(img):
+def renamer7(img: ee.Image) -> ee.Image:
+    """
+    Function to extract the Landsat 5 and 7 bands of interest and rename it
+    to coloquial names.
+
+    Parameters
+    ----------
+    img : ee.Image
+        Image of interest to subset and rename.
+
+    Returns
+    -------
+    img : ee.Image
+        Image with bands filtereds and renamed.
+    """
     ori_names = ["SR_B1", "SR_B2", "SR_B3", "SR_B4", "ST_B6"]
     new_names = ["BLUE", "GREEN", "RED", "NIR", "TEMPERATURE"]
     
@@ -8,7 +22,21 @@ def renamer7(img):
 
     return img
 
-def renamer8(img):
+def renamer8(img: ee.Image) -> ee.Image:
+    """
+    Function to extract the Landsat 8 bands of interest and rename it
+    to coloquial names.
+
+    Parameters
+    ----------
+    img : ee.Image
+        Image of interest to subset and rename.
+
+    Returns
+    -------
+    img : ee.Image
+        Image with bands filtereds and renamed.
+    """
     ori_names = ["SR_B2", "SR_B3", "SR_B4", "SR_B5", "ST_B10"]
     new_names = ["BLUE", "GREEN", "RED", "NIR", "TEMPERATURE"]
     
@@ -16,7 +44,20 @@ def renamer8(img):
 
     return img
 
-def calc_ndvi(img):
+def calc_ndvi(img: ee.Image) -> ee.Image:
+    """
+    Function to calculate de NDVI in one image.
+
+    Parameters
+    ----------
+    img : ee.Image
+        Image of interest to calculate the NDVI.
+
+    Returns
+    -------
+    img : ee.Image
+        Image with NDVI calculated.
+    """
     f = "(b('NIR')-b('RED'))/(b('NIR')+b('RED'))"
     ndvi = img.expression(f).rename("NDVI")
 
