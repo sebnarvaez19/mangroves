21f03cf55e511d26586d24c2004e92313b8ce922
srnarvaez <sebnarvaez19@gmail.com>
2022-12-27 13:54:44 -0500

    add function to calcualte the correlation matrix and update function to its plot

--- src/functions/stat_utils.py
+++ src/functions/stat_utils.py
@@ -2,9 +2,10 @@
 import numpy as np
 import pandas as pd
 from statsmodels.tsa.seasonal import seasonal_decompose
+from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
-from matplotlib.ticker import MultipleLocator
+from matplotlib.ticker import MultipleLocator, NullLocator
 from matplotlib.dates import YearLocator
 
 # %% Typing imports
@@ -120,6 +121,74 @@ def detrend_variables(
         dat2[variable] = components.observed - components.trend
 
     return dat2
+
+
+def corr_matrix(
+    data: pd.DataFrame,
+    variables: npt.ArrayLike | None == None,
+    half: bool = False,
+    hide_insignificants: bool = False,
+    singificant_threshold: float = 0.05,
+) -> npt.ArrayLike:
+    """
+    Calculate the pearson correlation matrix of the variables in a dataframe.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Dataframe with the variables to evaluate their correlation.
+
+    variables : ArrayLike | None = None
+        The variables of interest, if it is not defined, all variables in
+        the dataframe will be evaluated.
+
+    half : bool = False
+        If True, only show the corerlation of the first half of the matrix,
+        excluding the repeated correlation.
+
+    hide_insignifcants : bool = False
+        If True, hide all the correlation with a p-value greater than the
+        significant threshold.
+
+    siginificant_threshold : float = 0.05
+        Threshold of significant correlation.
+
+    returns
+    -------
+    corr : pd.DataFrame
+        Dataframe with the correlation values.
+
+    """
+    if variables == None:
+        variables = data.columns
+
+    reverse = variables[::-1]
+
+    N = len(variables)
+
+    corr = np.empty((N, N))
+    pval = np.full((N, N), np.nan)
+    mask = np.full((N, N), np.nan)
+
+    for i, iv in enumerate(variables):
+        for j, jv in enumerate(reverse):
+            c, p = pearsonr(data[iv], data[jv])
+            corr[j, i] = c
+
+            if p <= singificant_threshold:
+                pval[j, i] = 1.0
+
+        mask[: N - i, i] = 1.0
+
+    if half:
+        corr *= mask
+
+    if hide_insignificants:
+        corr *= pval
+
+    corr = pd.DataFrame(data=corr, index=reverse, columns=variables)
+
+    return corr
 
 
 def plot_ts_components(
@@ -345,7 +414,7 @@ def plot_acf_ccf(
         # Set ylims and ticks
         ax.set_ylim(bottom=ylims[0], top=ylims[1])
         ax.xaxis.set_major_locator(MultipleLocator(4))
-        ax.xaxis.set_major_locator(MultipleLocator(4))
+        ax.xaxis.set_minor_locator(MultipleLocator(1))
 
     # Share all x and y axis
     axs[0].get_shared_x_axes().join(*axs)
@@ -353,53 +422,118 @@ def plot_acf_ccf(
 
     return fig
 
-def plot_corr_matrix(data: pd.DataFrame, variables: npt.ArrayLike | None = None) -> Figure:
+
+def plot_corr_matrix(
+    data: pd.DataFrame,
+    variables: npt.ArrayLike | None == None,
+    half: bool = False,
+    hide_insignificants: bool = False,
+    singificant_threshold: float = 0.05,
+    show_labels: bool = True,
+    show_colorbar: bool = False,
+    palette: str = "Spectral",
+    text_color: str = "black",
+) -> Figure:
     """
-    Function to plot corr matrix.
+    Calculate the pearson correlation matrix of the variables in a dataframe.
 
     Parameters
     ----------
     data : pd.DataFrame
-        Dataframe with the variables to plot.
+        Dataframe with the variables to evaluate their correlation.
 
     variables : ArrayLike | None = None
-        Variables of interest.
+        The variables of interest, if it is not defined, all variables in
+        the dataframe will be evaluated.
 
-    Returns
+    half : bool = False
+        If True, only show the corerlation of the first half of the matrix,
+        excluding the repeated correlation.
+
+    hide_insignifcants : bool = False
+        If True, hide all the correlation with a p-value greater than the
+        significant threshold.
+
+    siginificant_threshold : float = 0.05
+        Threshold of significant correlation.
+
+    show_labels : bool = True
+        Show the correlation value.
+
+    show_colorbar : bool = False
+        Show colorbar.
+
+    palette : str = Spectral
+        Color palette for correlation plot.
+
+    text_color : str = black
+        Color of text correlation labels.
+
+    returns
     -------
-    fig : matplotlib.figure.Figure
-        Corr matrix plot.
+    corr : pd.DataFrame
+        Dataframe with the correlation values.
 
     """
-    # If variables isn't defined, get the columns of the dataframe
+
+    # If variables are not defined get all columns from data
     if variables == None:
         variables = data.columns
-    
+
+    # Get the number of variables
+    N = len(variables)
+
+    # Reverse variables for plot
+    reverse = variables[::-1]
+
     # Get the correlation matrix
-    corr = data[variables].corr()
+    corr = corr_matrix(
+        data, variables, half, hide_insignificants, singificant_threshold
+    )
+
+    if show_colorbar:
+        fs = (4, 3)
+    else:
+        fs = (3, 3)
 
     # Create the figure and the axes
-    fig = plt.figure(figsize=(4, 3))
+    fig = plt.figure(figsize=fs)
     ax1 = plt.subplot(1, 1, 1)
 
     # Plot matrix with pcolormesh
     im1 = ax1.pcolormesh(
-        variables, variables, corr, cmap="Spectral", edgecolor="w", vmin=-1, vmax=1
+        variables, reverse, corr, cmap=palette, edgecolor="w", vmin=-1, vmax=1
     )
 
+    # Invert y axis
+    ax1.invert_yaxis()
+
     # Add the colorbar
-    cax = ax1.inset_axes([1.04, 0.1, 0.05, 0.8])
-    bar = plt.colorbar(im1, cax=cax, label="Correlation")
+    if show_colorbar:
+        cax = ax1.inset_axes([1.04, 0.1, 0.05, 0.8])
+        bar = plt.colorbar(im1, cax=cax, label="Correlation")
 
-    x, y = np.meshgrid([0, 1, 2, 3], [0, 1, 2, 3])
-    x = x.reshape(-1)
-    y = y.reshape(-1)
-    t = corr.values.reshape(-1)
+    if show_labels:
+        x, y = np.meshgrid(np.arange(N), np.arange(N))
+        x = x.reshape(-1)
+        y = y.reshape(-1)
+        t = corr.values.reshape(-1)
 
-    for xi, yi, ti in zip(x, y, t):
-        ax1.text(xi, yi, round(ti, 2), size=8, ha="center", va="center")
+        for xi, yi, ti in zip(x, y, t):
+            if np.isfinite(ti):
+                ax1.text(
+                    xi,
+                    yi,
+                    round(ti, 2),
+                    color=text_color,
+                    size=8,
+                    ha="center",
+                    va="center",
+                )
 
     # Rotate labels to improve their readability
-    ax1.set_xticklabels(variables, rotation = 30)
-    
+    ax1.set_xticklabels(variables, rotation=30)
+    ax1.xaxis.set_minor_locator(NullLocator())
+    ax1.yaxis.set_minor_locator(NullLocator())
+
     return fig
